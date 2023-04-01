6e2f7377570d64acbe5b96d7c4d65a57f45ab764
Jay7221 <jns29072003@gmail.com>
2023-04-01 17:56:21 +0530

    Debugged Problem 3

--- 3_Print_Your_Tree.cpp
+++ 3_Print_Your_Tree.cpp
@@ -2,91 +2,115 @@
 
 // You are given a basic implementation of Binary Tree, which consists of some errors (Syntactical/Logical). You need to detect those errors and correct so that, they satisfy the given input output conditions.
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-class Node{
+class Node
+{
 
-  public:
-    int data;
-    Node *left, *right;
+public:
+  int data;
+  Node *left, *right;
 
-    Node(int data) {
-      this.data = data;
-      this.left = this.right = NULL;
+  Node()
+  {
+    this->left = this->right = NULL;
+  }
+
+  Node(int data)
+  {
+    this->data = data;
+    this->left = this->right = NULL;
+  }
+
+  // Function to create a node.
+  Node *add(int value, Node *root)
+  {
+    if (root == NULL)
+    {
+      return new Node(value);
     }
 
-    // Function to create a node.
-    Node* add(Node *root, int value) {
-      if(root == NULL) {
-        return new Node(value);
-      }
-
-      if(value < root.data) {
-        root.left = add(value, root);
-      }
-      else{
-        root.right = add(value, root);
-      }
+    if (value < root->data)
+    {
+      root->left = add(value, root->left);
     }
+    else
+    {
+      root->right = add(value, root->right);
+    }
+    return root;
+  }
 };
 
 // Function to return preorder traversal of binary tree.
-vector<int> preorder(Node* root) {
+vector<int> preorder(Node *root)
+{
   vector<int> ans;
-  if(root == NULL){
-      return ans;
+  if (root == NULL)
+  {
+    return ans;
   }
 
-  queue<Node*> stk;
+  stack<Node *> stk;
   stk.push(root);
 
-  while(!stk.empty()){
-      Node* temp = stk.front();
-      stk.pop();
+  while (!stk.empty())
+  {
+    Node *temp = stk.top();
+    stk.pop();
 
-      if(temp->left){
-          stk.push(temp->left);
-      }
-      if(temp->right){
-          stk.push(temp->right);
-      }
+    
+    if (temp->right)
+    {
+      stk.push(temp->right);
+    }
+    if (temp->left)
+    {
+      stk.push(temp->left);
+    }
 
-      ans.push_back(temp->data);
+    ans.push_back(temp->data);
   }
 
   return ans;
 }
 
 // Function to return postorder traversal of binary tree.
-vector<int> postorderTraversal(Node* root) {
-    vector <int> ans;
-    if(root == NULL){
-        return ans;
-    }
-    
-    queue <Node*> q;
-    q.push(root);
-    
-    while(!q.empty()){
-        Node* temp = q.front();
-        q.pop();
-        if(temp->right){
-            q.push(temp->right);
-        }
-        if(temp->left){
-            q.push(temp->left);
-        }
-        ans.push_back(temp->val);
-    }
-    reverse(ans.begin(), ans.end());
+vector<int> postorderTraversal(Node *root)
+{
+  vector<int> ans;
+  if (root == NULL)
+  {
     return ans;
+  }
+
+  stack<Node *> q;
+  q.push(root);
+
+  while (!q.empty())
+  {
+    Node *temp = q.top();
+    q.pop();
+    
+    if (temp->left)
+    {
+      q.push(temp->left);
+    }
+    if (temp->right)
+    {
+      q.push(temp->right);
+    }
+    ans.push_back(temp->data);
+  }
+  reverse(ans.begin(), ans.end());
+  return ans;
 }
 
 int main()
 {
-  
-  Node* root = NULL;
+
+  Node *root = NULL;
   Node bt;
 
   root = bt.add(12, root);
@@ -96,13 +120,15 @@ int main()
   root = bt.add(9, root);
 
   auto ans1 = preorder(root);
-  for(auto itr:ans1) {
-    cout<<itr<<" ";
+  for (auto itr : ans1)
+  {
+    cout << itr << " ";
   }
-
+  cout << '\n';
   auto ans2 = postorderTraversal(root);
-  for(auto itr:ans2) {
-    cout<<itr<<" ";
+  for (auto itr : ans2)
+  {
+    cout << itr << " ";
   }
   return 0;
 }
