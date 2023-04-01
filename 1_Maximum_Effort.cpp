c588340915009c22815332a38805089a3dd5202c
Jay7221 <jns29072003@gmail.com>
2023-04-01 17:32:44 +0530

    Debugged Problem 1

--- 1_Maximum_Effort.cpp
+++ 1_Maximum_Effort.cpp
@@ -9,11 +9,12 @@
  *
  */
 #include <iostream>
+using namespace std;
 
 int maxValue(int *arr, int size)
 {
     int *max = arr;
-    for (int i = 1; i < size; i++)
+    for (int i = 1; i <= size; i++)
     {
         if (*arr > *max)
         {
@@ -21,13 +22,13 @@ int maxValue(int *arr, int size)
         }
         arr++;
     }
-    return max;
+    return *max;
 }
 
 int main()
 {
     int arr[] = {10, 20, 5, 30, 15};
-    int size = sizeof(arr) / sizeof(arr);
+    int size = sizeof(arr) / sizeof(arr[0]);
 
     int max = maxValue(arr, size);
 
